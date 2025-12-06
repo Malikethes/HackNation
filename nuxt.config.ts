220e9eee4f801c838057964441c863ac55818f04
Kitava <malynovskydmitriyy@gmail.com>
2025-12-06 14:24:18 +0100

    chore: init Vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,39 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image'
-  ]
+  ssr: false,
+  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon', 'vuetify-nuxt-module'],
+  vuetify: {
+    vuetifyOptions: {
+      theme: {
+        defaultTheme: 'light',
+        themes: {
+          dark: {
+            colors: {
+              primary: '#307de8',
+              secondary: '#424242',
+              accent: '#82B1FF',
+              error: '#FF5252',
+              info: '#2196F3',
+              success: '#4CAF50',
+              warning: '#FFC107',
+              background: '#111821',
+              surface: '#1e293b',
+            },
+          },
+          light: {
+            colors: {
+              primary: '#307de8',
+              secondary: '#424242',
+              background: '#f6f7f8',
+              surface: '#ffffff',
+            },
+          },
+        },
+      },
+      icons: {
+        defaultSet: 'mdi',
+      },
+    },
+  },
 })
